81118b308d12441bfd082a05eef33c135f1502d0
vickbk <vickbake8@gmail.com>
2025-11-14 13:38:32 +0000

    Destination page data filling

--- components/destination/DestinationMain.tsx
+++ components/destination/DestinationMain.tsx
@@ -1,7 +1,55 @@
+"use client";
 import PagesTitle from "@components/shared/PagesTitle";
+import * as data from "@utils/starter-code/data.json";
+import { useState } from "react";
 
 export default function DestinationMain() {
-  return <>
-  <PagesTitle title={["01","Pick your destination"]} />
-  </>;
+  const { destinations } = data;
+  const [activeIndex, setActiveIndex] = useState(0);
+  const {
+    name,
+    description,
+    images: { webp: image },
+    travel,
+    distance,
+  } = destinations[activeIndex];
+  return (
+    <>
+      <PagesTitle title={["01", "Pick your destination"]} />
+      <section>
+        <article>
+          <h2>Planet illustration</h2>
+          <img src={image} alt={`Planet ${name} preview image`} />
+        </article>
+        <article>
+          <nav>
+            <ul>
+              {destinations.map(({ name }, index) => (
+                <li key={index}>
+                  <button type="button" onClick={() => setActiveIndex(index)}>
+                    {name}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <h2>
+            <span>The planet </span> {name}
+          </h2>
+          <p>{description}</p>
+          <div>
+            {[
+              ["avg. distance", distance],
+              ["est. travel time", travel],
+            ].map(([title, value], index) => (
+              <div key={index}>
+                <div>{title}</div>
+                <div>{value}</div>
+              </div>
+            ))}
+          </div>
+        </article>
+      </section>
+    </>
+  );
 }
