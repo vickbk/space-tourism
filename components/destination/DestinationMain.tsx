0ed03d38969c2886d9b0a8474b3c21ffcb30843e
vickbk <vickbake8@gmail.com>
2025-11-18 14:58:21 +0200

    Destination page tablet design

--- components/destination/DestinationMain.tsx
+++ components/destination/DestinationMain.tsx
@@ -1,6 +1,6 @@
 "use client";
 import PagesTitle from "@components/shared/PagesTitle";
-import { CSSProperties, useState } from "react";
+import { useState } from "react";
 import { loadData } from "@lib/load-data";
 import { DefaultContainer } from "@components/shared/DefaultContainer";
 
@@ -20,45 +20,50 @@ export default function DestinationMain() {
       <section className="planet c-blue-300">
         <article>
           <h2 className="sr-only">Planet illustration</h2>
-          <div className="px-12 py-16">
-            <img src={image} alt={`Planet ${name} preview image`} />
+          <div className="py-16">
+            <img
+              className="mx-auto w-75"
+              src={image}
+              alt={`Planet ${name} preview image`}
+            />
           </div>
         </article>
         <article>
-          <nav>
-            <ul
-              className="flex justify-center gap-8 ff-barlow-cond"
-              aria-label="Planet destination list"
-            >
-              {destinations.map(({ name }, index) => (
-                <li key={index}>
-                  <button
-                    className="planet__choice"
-                    type="button"
-                    onClick={() => setActiveIndex(index)}
-                    data-current={activeIndex === index}
-                  >
-                    {name}
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </nav>
-          <h2 className="planet__title">
-            <span className="sr-only">The planet </span> {name}
-          </h2>
-          <p className="pb-8 border-b">{description}</p>
-          <div className="pt-8 uppercase">
-            {[
-              ["avg. distance", distance],
-              ["est. travel time", travel],
-            ].map(([title, value], index) => (
-              <div key={index} className="">
-                <div className="ff-barlow-cond">{title}</div>
-                <div className="text-3xl ff-bellefair c-white">{value}</div>
-              </div>
+          <ul
+            className="flex justify-center gap-8 ff-barlow-cond"
+            aria-label="Planet destination list"
+          >
+            {destinations.map(({ name }, index) => (
+              <li key={index}>
+                <button
+                  className="planet__choice"
+                  type="button"
+                  onClick={() => setActiveIndex(index)}
+                  data-current={activeIndex === index}
+                >
+                  {name}
+                </button>
+              </li>
             ))}
-          </div>
+          </ul>
+
+          <section className="max-w-162 mx-auto">
+            <h2 className="planet__title">
+              <span className="sr-only">The planet </span> {name}
+            </h2>
+            <p className="planet__separator">{description}</p>
+            <div className="pt-8 uppercase sm:flex justify-evenly">
+              {[
+                ["avg. distance", distance],
+                ["est. travel time", travel],
+              ].map(([title, value], index) => (
+                <div key={index} className="">
+                  <div className="ff-barlow-cond">{title}</div>
+                  <div className="text-3xl ff-bellefair c-white">{value}</div>
+                </div>
+              ))}
+            </div>
+          </section>
         </article>
       </section>
     </DefaultContainer>
