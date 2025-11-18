88a58365ef38e2cfb7b69bd8c36aa8cf6b4f820c
vickbk <vickbake8@gmail.com>
2025-11-18 20:19:49 +0200

    home page text desktop design

--- components/shared/DefaultContainer.tsx
+++ components/shared/DefaultContainer.tsx
@@ -1,5 +1,9 @@
 import { ReactNode } from "react";
 
 export function DefaultContainer({ children }: { children: ReactNode }) {
-  return <section className="px-8 text-center c-blue-300">{children}</section>;
+  return (
+    <section className="px-8 text-center c-blue-300 md:text-start md:grid grid-cols-2 max-w-300 mx-auto">
+      {children}
+    </section>
+  );
 }
