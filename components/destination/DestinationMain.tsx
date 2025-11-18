cd97b316019a887bc59d03c9d396fdcc4323148f
vickbk <vickbake8@gmail.com>
2025-11-18 22:11:51 +0200

    Desktop design for destination page

--- components/destination/DestinationMain.tsx
+++ components/destination/DestinationMain.tsx
@@ -18,55 +18,56 @@ export default function DestinationMain() {
   return (
     <DefaultContainer>
       <PagesTitle title={"01 Pick your destination"} />
-      <section className="planet c-blue-300">
-        <article>
-          <h2 className="sr-only">Planet illustration</h2>
-          <div className="py-16">
-            <img
-              className="mx-auto w-75"
-              src={image}
-              alt={`Planet ${name} preview image`}
-            />
-          </div>
-        </article>
-        <article>
-          <ul
-            className="flex justify-center gap-8 ff-barlow-cond"
-            aria-label="Planet destination list"
-          >
-            {destinations.map(({ name }, index) => (
-              <li key={index}>
-                <button
-                  className="planet__choice"
-                  type="button"
-                  onClick={() => setActiveIndex(index)}
-                  data-current={activeIndex === index}
-                >
-                  {name}
-                </button>
-              </li>
-            ))}
-          </ul>
 
-          <ContentHolder>
-            <h2 className="planet__title">
-              <span className="sr-only">The planet </span> {name}
-            </h2>
-            <p className="planet__separator">{description}</p>
-            <div className="py-8 uppercase sm:flex justify-evenly">
-              {[
-                ["avg. distance", distance],
-                ["est. travel time", travel],
-              ].map(([title, value], index) => (
-                <div key={index} className="">
-                  <div className="ff-barlow-cond">{title}</div>
-                  <div className="text-3xl ff-bellefair c-white">{value}</div>
-                </div>
-              ))}
-            </div>
-          </ContentHolder>
-        </article>
-      </section>
+      {/* <section className="planet"> */}
+      <article>
+        <h2 className="sr-only">Planet illustration</h2>
+        <div className="py-16">
+          <img
+            className="mx-auto w-75"
+            src={image}
+            alt={`Planet ${name} preview image`}
+          />
+        </div>
+      </article>
+      <article>
+        <ul
+          className="flex justify-center md:justify-start gap-8 ff-barlow-cond"
+          aria-label="Planet destination list"
+        >
+          {destinations.map(({ name }, index) => (
+            <li key={index}>
+              <button
+                className="planet__choice"
+                type="button"
+                onClick={() => setActiveIndex(index)}
+                data-current={activeIndex === index}
+              >
+                {name}
+              </button>
+            </li>
+          ))}
+        </ul>
+
+        <ContentHolder>
+          <h2 className="planet__title">
+            <span className="sr-only">The planet </span> {name}
+          </h2>
+          <p className="planet__separator">{description}</p>
+          <div className="py-8 uppercase sm:flex justify-evenly md:justify-start">
+            {[
+              ["avg. distance", distance],
+              ["est. travel time", travel],
+            ].map(([title, value], index) => (
+              <div key={index} className={`${index === 1 ? "mx-auto" : ""}`}>
+                <div className="ff-barlow-cond">{title}</div>
+                <div className="text-3xl ff-bellefair c-white">{value}</div>
+              </div>
+            ))}
+          </div>
+        </ContentHolder>
+      </article>
+      {/* </section> */}
     </DefaultContainer>
   );
 }
--- components/shared/DefaultContainer.tsx
+++ components/shared/DefaultContainer.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export function DefaultContainer({ children }: { children: ReactNode }) {
   return (
-    <section className="px-8 text-center c-blue-300 md:text-start md:grid grid-cols-2 max-w-300 mx-auto">
+    <section className="px-8 text-center c-blue-300 md:text-start md:grid grid-cols-2 gap-y-16 max-w-300 mx-auto">
       {children}
     </section>
   );
