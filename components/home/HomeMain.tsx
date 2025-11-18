7063bb4cc85e32c0effbf1b3ad05510444c94799
vickbk <vickbake8@gmail.com>
2025-11-18 14:24:34 +0200

    Home design finalization

--- components/home/HomeMain.tsx
+++ components/home/HomeMain.tsx
@@ -9,7 +9,7 @@ export default function HomeMain() {
         <h1 className="title-big uppercase">
           <PageTitle title={["So, you want to travel to", "Space"]} />
         </h1>
-        <p>
+        <p className="max-w-162 mx-auto">
           Let’s face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
           back, and relax because we’ll give you a truly out of this world
